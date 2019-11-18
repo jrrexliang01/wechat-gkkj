3c30c96be436645e170fc2d043357841e2a7dc15
zhangyongliang <jrrexliang@live.com>
2019-11-18 22:49:41 +0800

    格式化代码

--- src/config/index.js
+++ src/config/index.js
@@ -2,153 +2,188 @@ import API from '../api'
 
 const getNewList = async () => {
   const res = (await API.getNewList()).data.data.content
-  return { res }
+  const newList = res
+  return { newList }
 }
 const getDocList = async () => {
   const res = (await API.getDocList()).data.data.content
-  return { res }
+  const docList = res
+  return { docList }
 }
 const getDocInfo = async (docId) => {
   const res = (await API.getDocInfo(docId)).data.data
-  return { res }
+  const docInfo = res
+  return { docInfo }
 }
 const getNewsInfo = async (newsId) => {
   const res = (await API.getNewsInfo(newsId)).data.data
-  return { res }
+  const newsInfo = res
+  return { newsInfo }
 }
 const getOrderList = async (patId) => {
   const res = (await API.getOrderList(patId)).data.data.content
-  return { res }
+  const orderList = res
+  return { orderList }
 }
 const getOrderInfo = async (ordId) => {
   const res = (await API.getOrderInfo(ordId)).data.data
-  return { res }
+  const orderInfo = res
+  return { orderInfo }
 }
 const getOrderCourseList = async (docId) => {
   const res = (await API.getOrderCourseList(docId)).data.data.content
-  return { res }
+  const orderCourseList = res
+  return { orderCourseList }
 }
 const getOrderCourseInfo = async (ordId) => {
   const res = (await API.getOrderCourseInfo(ordId)).data.data
-  return { res }
+  const orderCourseInfo = res
+  return { orderCourseInfo }
 }
 const getOrderStatusList = async (patId, status) => {
   const res = (await API.getOrderStatusList(patId, status)).data.data.content
-  return { res }
+  const orderList = res
+  return { orderList }
 }
 const getOrderCourseStatusList = async (docId, status) => {
   const res = (await API.getOrderCourseStatusList(docId, status)).data.data.content
-  return { res }
+  const orderCourseList = res
+  return { orderCourseList }
 }
 const getAgreementInfo = async () => {
   const res = (await API.getAgreementInfo()).data.data
-  return { res }
+  const agreementInfo = res
+  return { agreementInfo }
 }
 const getPatReportList = async (patId) => {
   const res = (await API.getPatReportList(patId)).data.data.content
-  return { res }
+  const reportList = res
+  return { reportList }
 }
 const reportAdd = async (formData) => {
   const res = (await API.reportAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const getReportInfo = async (reportId) => {
   const res = (await API.getReportInfo(reportId)).data.data
-  return { res }
+  const reportInfo = res
+  return { reportInfo }
 }
 const searchAdd = async (formData) => {
   const res = (await API.searchAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const searchHot = async () => {
   const res = (await API.searchHot()).data.data
-  return { res }
+  const searchHot = res
+  return { searchHot }
 }
 const getMsgList = async (userId, userType) => {
   const res = (await API.getMsgList(userId, userType)).data.data.content
-  return { res }
+  const msgList = res
+  return { msgList }
 }
 const msgAdd = async (formData) => {
   const res = (await API.msgAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const patientAdd = async (formData) => {
   const res = (await API.patientAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const doctorAdd = async (formData) => {
   const res = (await API.doctorAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const orderAdd = async (formData) => {
   const res = (await API.orderAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const getMyPat = async (docId) => {
   const res = (await API.getMyPat(docId)).data.data
-  return { res }
+  const myPatInfo = res
+  return { myPatInfo }
 }
 const getCourseList = async (courseTitle) => {
   const res = (await API.getCourseList(courseTitle)).data.data.content
-  return { res }
+  const courseList = res
+  return { courseList }
 }
 const getCourseInfo = async (couresId) => {
   const res = (await API.getCourseInfo(couresId)).data.data
-  return { res }
+  const courseInfo = res
+  return { courseInfo }
 }
 const orderCourseAdd = async (formData) => {
   const res = (await API.orderCourseAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const getArticleList = async (courseTitle) => {
   const res = (await API.getCourseList(courseTitle)).data.data.content
-  return { res }
+  const articleList = res
+  return { articleList }
 }
 const getArticleInfo = async (articleId) => {
   const res = (await API.getArticleInfo(articleId)).data.data
-  return { res }
+  const articleInfo = res
+  return { articleInfo }
 }
 const remindAdd = async (formData) => {
   const res = (await API.remindAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const remindConfigAdd = async (formData) => {
   const res = (await API.remindConfigAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const getremindList = async (docId) => {
   const res = (await API.getCourseList(docId)).data.data.content
-  return { res }
+  const remindList = res
+  return { remindList }
 }
 const getRemindInfo = async (remindId) => {
   const res = (await API.getArticleInfo(remindId)).data.data
-  return { res }
+  const remindInfo = res
+  return { remindInfo }
 }
 const getCodeMsg = async (docId, code) => {
   const res = (await API.getCodeMsg(docId, code)).data.msg
-  return { res }
+  const codeMsg = res
+  return { codeMsg }
 }
 const studyAdd = async (formData) => {
   const res = (await API.studyAdd(formData)).data.msg
-  return { res }
+  const msg = res
+  return { msg }
 }
 const getHotDocList = async () => {
   const res = (await API.getHotDocList()).data.data.content
-  return { res }
+  const hotDocList = res
+  return { hotDocList }
 }
-
 const getBanner = async () => {
   const res = (await API.getBanner()).data.data.content
-  return { res }
+  const banner = res
+  return { banner }
 }
 const getDocBill = async (docId) => {
   const res = (await API.getDocBill(docId)).data.data.content
-  return { res }
+  const docBill = res
+  return { docBill }
 }
-
 const getPatientDetail = async (patientId) => {
   const res = (await API.getPatientDetail(patientId)).data.data.content
-  return { res }
+  const patientDetail = res
+  return { patientDetail }
 }
 
 export {
