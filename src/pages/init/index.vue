35ee683f17f7f01df2f4f0ca3c6ffa02ede7a3ca
zhangyongliang <jrrexliang@live.com>
2019-11-20 16:21:36 +0800

    添加注册页面及相关业务逻辑

--- src/pages/init/index.vue
+++ src/pages/init/index.vue
@@ -34,58 +34,54 @@ export default {
             success (res) {
               var sessionKey = res.data
               wx.setStorageSync('sessionKey', sessionKey)
-              wx.getUserInfo({
-                success () {
-                  wx.request({
-                    url: 'https://gkkj.jrrexliang.com/api/wx/doc/userType',
-                    data: {
-                      openId: sessionKey.openid
-                    },
-                    method: 'POST',
-                    success (res) {
-                      console.log(res)
-                      if (res.data.data.userType === null) {
-                        console.log('null')
-                        wx.redirectTo({
-                          url: '/pages/home/main'
-                        })
-                      } else if (res.data.data.userType === 1) {
-                        console.log('doc')
-                        wx.setStorageSync('userInfo', res.data.data.doc)
-                        let options = genTestUserSig(res.data.data.doc.openId)
-                        options.runLoopNetType = 0
-                        if (options) {
-                          wx.$app.login({
-                            userID: res.data.data.doc.openId,
-                            userSig: options.userSig,
-                            hasUserInfo: true
-                          }).then(() => {
-                            wx.navigateTo({url: '/pages/home/main'})
-                          })
-                        }
-                        wx.redirectTo({
-                          url: '/pages/doc/home/main'
-                        })
-                      } else if (res.data.data.userType === 2) {
-                        console.log('patient')
-                        wx.setStorageSync('userInfo', res.data.data.patient)
-                        let options = genTestUserSig(res.data.data.patient.openId)
-                        options.runLoopNetType = 0
-                        if (options) {
-                          wx.$app.login({
-                            userID: res.data.data.patient.openId,
-                            userSig: options.userSig,
-                            hasUserInfo: true
-                          }).then(() => {
-                            wx.navigateTo({url: '/pages/home/main'})
-                          })
-                        }
-                        wx.redirectTo({
-                          url: '/pages/home/main'
-                        })
-                      }
+              wx.request({
+                url: 'https://gkkj.jrrexliang.com/api/wx/doc/userType',
+                data: {
+                  openId: sessionKey.openid
+                },
+                method: 'POST',
+                success (res) {
+                  console.log(res)
+                  if (res.data.data.userType === null) {
+                    console.log('null')
+                    wx.redirectTo({
+                      url: '/pages/home/main'
+                    })
+                  } else if (res.data.data.userType === 1) {
+                    console.log('doc')
+                    wx.setStorageSync('userInfo', res.data.data.doc)
+                    let options = genTestUserSig(res.data.data.doc.openId)
+                    options.runLoopNetType = 0
+                    if (options) {
+                      wx.$app.login({
+                        userID: res.data.data.doc.openId,
+                        userSig: options.userSig,
+                        hasUserInfo: true
+                      }).then(() => {
+                        wx.navigateTo({url: '/pages/home/main'})
+                      })
                     }
-                  })
+                    wx.redirectTo({
+                      url: '/pages/doc/home/main'
+                    })
+                  } else if (res.data.data.userType === 2) {
+                    console.log('patient')
+                    wx.setStorageSync('userInfo', res.data.data.patient)
+                    let options = genTestUserSig(res.data.data.patient.openId)
+                    options.runLoopNetType = 0
+                    if (options) {
+                      wx.$app.login({
+                        userID: res.data.data.patient.openId,
+                        userSig: options.userSig,
+                        hasUserInfo: true
+                      }).then(() => {
+                        wx.navigateTo({url: '/pages/home/main'})
+                      })
+                    }
+                    wx.redirectTo({
+                      url: '/pages/home/main'
+                    })
+                  }
                 }
               })
             }
