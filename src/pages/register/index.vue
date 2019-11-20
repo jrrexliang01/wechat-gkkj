1b823a59f4368784fa2f5467cde287f3a0b8e606
zhangyongliang <jrrexliang@live.com>
2019-11-20 19:15:58 +0800

    修改注册页面样式

--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="container ub-box ub-col">
-    <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff" style="border-bottom: 1px solid #f5f5f5">
-      <p class="z-font-size-20 z-color-888" style="color: #5ca2f2;padding: 10px;">患者注册</p>
+    <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff">
+      <p class="z-font-size-18 z-color-000" style="padding: 20px 0 20px 20px;">请输入手机号</p>
     </dd>
-    <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px" style="border-bottom: 1px solid #f5f5f5">
+    <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px">
       <p class="ub-box">
-        <span class="z-font-size-14 z-color-333"><i-input value="" v-model="phone" title="手机号"/></span>
+        <span class="z-font-size-14 z-color-333"><i-input value="" v-model="phone" type="number" autofocus placeholder="请输入手机号" title="+86" right/></span>
       </p>
       <p class="ub-box">
-        <span class="z-font-size-14 z-color-333"><i-input value="" v-model="code" title="邀请码"/></span>
+        <span class="z-font-size-14 z-color-333"><i-input value="" v-model="code" type="number" title="邀请码" placeholder="请输入邀请码" right/></span>
       </p>
     </dd>
-    <div class="revise">
-      <button @click="saveUser()" class="btn" style="background-color: #5ca2f2;margin: 20px;">注册</button>
-    </div>
+    <dd class="z-font-size-18 z-color-333 z-padding-h-10-px z-margin-top-30-px">
+      <button class="loginBtn" lang="zh_CN" @click="saveUser()">注册</button>
+    </dd>
+    <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px ub-box ub-between" style="padding: 5px 20px 0 20px;">
+      <p class="z-font-size-14 z-color-333">注册代表同意用户协议</p>
+      <p class="ub-box">
+        <span class="z-font-size-14 z-color-333" style="color: #357cfb;">我是医生</span>
+      </p>
+    </dd>
   </div>
 </template>
 
@@ -58,4 +64,5 @@ export default {
 <style lang="stylus" scoped>
   .container{width:100%;height:100vh;background:#ffffff}
   .label{border-radius:3px;background: #fff;padding: 3px 5px;margin: 0 5px 5px 0}
+  .loginBtn{font-size:18px;color:#fff;padding:0px 20px;margin-left: 10px;margin-right: 10px;background: #357cfb}
 </style>
