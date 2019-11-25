038c915f3c9edbd05c3beef399baa41786a05479
zhangyongliang <jrrexliang@live.com>
2019-11-25 20:51:31 +0800

    添加主讲医生

--- src/pages/doc/courseInfo/index.vue
+++ src/pages/doc/courseInfo/index.vue
@@ -11,17 +11,6 @@
           </image>
         </dd>
       </dl>
-      <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px ub-box ub-between" style="border-bottom: 1px solid #f5f5f5">
-        <p class="ub-box ub-ver">
-          <span class="z-font-size-24 z-margin-right-5-px" style="color:#06c1ae">¥{{courseInfo.price}}</span>
-          <span class="z-font-size-13 z-color-888"></span>
-        </p>
-        <p class="ub-box ub-ver">
-				<span @click.stop="toConsult (courseInfo.id) " class="buyBtn ub-box ub-ver z-font-size-16 z-color-fff">
-					立即购买
-				</span>
-        </p>
-      </dd>
       <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px ub-box ub-ver" style="border-bottom: 1px solid #f5f5f5">
         <p class="z-font-size-14 z-color-888">介绍</p>
       </dd>
@@ -29,6 +18,12 @@
         <p class="z-font-size-14 z-color-333"><b>课程类别</b></p>
         <p class="ub-box">
           <span class="z-font-size-14 z-color-333">{{typeVal[courseInfo.type]}}</span>
+        </p>
+      </dd>
+      <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px ub-box ub-between" style="border-bottom: 1px solid #f5f5f5">
+        <p class="z-font-size-14 z-color-333"><b>主讲医生</b></p>
+        <p class="ub-box">
+          <span class="z-font-size-14 z-color-333">{{courseInfo.doc.docName}}</span>
         </p>
       </dd>
       <dd class="z-width-100-percent z-box-sizing-border z-bg-color-fff z-padding-all-8-px ub-box ub-ver" style="border-bottom: 1px solid #f5f5f5">
@@ -39,6 +34,14 @@
           <li class="z-font-size-14 z-color-333 z-box-sizing-border z-lineHeight-24">{{courseInfo.summary}}</li>
         </ul>
       </dd>
+      <!--fixed部分-->
+      <ul class="fixCon ub-box ub-ver ub-between">
+        <li class="ub-box ub-ver z-padding-h-10-px">
+          <span class="z-font-size-12 z-color-999 z-margin-right-10-px">课程金额：</span>
+          <span class="z-font-size-18 z-font-weight-bold" style="color:red">¥{{courseInfo.price}}</span>
+        </li>
+        <li @click.stop="toConsult (courseInfo.id)" class="submitBtn ub-box ub-ver z-font-size-16 z-color-fff">立即购买</li>
+      </ul>
     </scroll-view>
   </div>
 </template>
@@ -56,7 +59,10 @@ export default {
         payNum: '',
         price: '',
         type: '',
-        summary: ''
+        summary: '',
+        doc: {
+          docName: ''
+        }
       },
       typeVal: {
         '1': '讲座',
@@ -96,6 +102,6 @@ export default {
   .indexImg-bk{position: absolute;bottom: 0;left: 0;z-index: 1;width: 100%;height: 30%;background: rgba(0,0,0,.3);padding: 5px 0px}
   .buyBtn{background: #f90;padding: 8px 12px;border-radius:3px}
   .label{border-radius:3px;background: #fff;padding: 3px 5px;margin: 0 5px 5px 0}
-  .tuijian{color: #f90;border:1px solid #f90;}
-  .butuijian{color: #999;border:1px solid #ddd;}
+  .fixCon{position: fixed;left: 0;bottom: 0;z-index: 10;width: 100%;background: #fff;}
+  .submitBtn{padding: 15px 35px;background: #5CA2F2;box-sizing: border-box;}
 </style>
