ec32e106f4b71a521ec23f30b6d1043e1d443b04
zhangyongliang <jrrexliang@live.com>
2019-12-22 22:22:24 +0800

    修改我的订单列表

--- src/pages/doc/myOrder/index.vue
+++ src/pages/doc/myOrder/index.vue
@@ -3,13 +3,13 @@
     <scroll-view scroll-y style="height:calc(100vh);" scroll-top="0">
       <dl class="z-width-100-percent ub-box ub-col">
         <dd @click.stop="$openWin('/pages/doc/orderInfo/main?ordId=' + val.id)" v-for="(val, idx) in orderList" :key="idx" class="order z-width-100-percent ub-box z-box-sizing-border">
-          <img class="z-img-cover" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573708198208&di=caad1e40da5a13dd206896e3494cffed&imgtype=0&src=http%3A%2F%2Fku.90sjimg.com%2Felement_origin_min_pic%2F17%2F12%2F27%2Fd4e58d34321d4d7be62b4a6d7682c211.jpg" />
+          <img class="z-img-cover" :src="imgSrc"/>
           <div class="ub-flex-1 z-padding-left-10-px ub-box ub-col">
             <span class="z-font-size-15 z-color-333 z-margin-bottom-3-px z-font-weight-bold">{{val.course.courseTitle}}</span>
             <span class="z-font-size-12 z-color-888 z-margin-bottom-3-px">主讲医生：{{val.doc.docName}}</span>
             <span class="z-font-size-12 z-color-888 z-margin-bottom-3-px">总价：{{val.total}}</span>
           </div>
-          <span class="z-font-size-14" style="color:#06c1ae">{{val.statusStr}}</span>
+          <span class="z-font-size-14" style="color:#06c1ae">{{payStatus[val.status]}}</span>
         </dd>
       </dl>
     </scroll-view>
@@ -17,43 +17,38 @@
 </template>
 
 <script>
-import { getOrderCourseList, getOrderCourseStatusList } from '../../../config'
-export default {
-  data () {
-    return {
-      modalLoading: true,
-      modal: false,
-      orderList: {},
-      docId: 0,
-      orderStatus: 0
-    }
-  },
-  methods: {
-  },
-  async onLoad (options) {
-    this.docId = parseInt(options.docId)
-    this.orderStatus = parseInt(options.orderStatus)
-    if (this.orderStatus === 2) {
-      const { orderCourseList } = await getOrderCourseList(this.docId)
-      wx.setStorageSync('orderCourseList', orderCourseList)
-    } else {
-      const { orderCourseList } = await getOrderCourseStatusList(this.docId, this.orderStatus)
-      wx.setStorageSync('orderCourseList', orderCourseList)
-    }
-    this.orderList = wx.getStorageSync('orderCourseList')
-    for (const v of this.orderList) {
-      if (v.status === 1) {
-        v.statusStr = '已付款'
+  import {getOrderCourseList, getOrderCourseStatusList} from '../../../config'
+
+  export default {
+    data () {
+      return {
+        orderList: [],
+        imgSrc: '',
+        payStatus: {
+          1: '已付款',
+          0: '未付款'
+        }
       }
-      if (v.status === 0) {
-        v.statusStr = '未付款'
+    },
+    async onLoad (options) {
+      if (parseInt(options.orderStatus) === 2) {
+        const {orderCourseList} = await getOrderCourseList(parseInt(options.docId))
+        this.orderList = orderCourseList
+      } else {
+        const {orderCourseList} = await getOrderCourseStatusList(parseInt(options.docId), parseInt(options.orderStatus))
+        this.orderList = orderCourseList
+      }
+      console.log(this.orderList)
+    },
+    mounted () {
+      let banner = wx.getStorageSync('enclosureList')
+      for (let i = 0; i < banner.data.length; i++) {
+        if (banner.data[i].enclosureName === 'course_order') {
+          this.imgSrc = banner.data[i].enclosurePath
+        }
       }
     }
-  },
-  mounted () {
-    this.orderList = wx.getStorageSync('orderList')
   }
-}
 </script>
 
 <style lang="stylus" scoped>
