0992232293156e86ddd2de711a32b99bc5768634
zhangyongliang <jrrexliang@live.com>
2019-12-22 12:55:31 +0800

    修改新闻列表的文章图片为或banner

--- src/pages/science/index.vue
+++ src/pages/science/index.vue
@@ -7,7 +7,7 @@
           <div class="ub-flex-1 z-padding-left-10-px ub-box ub-col z-padding-bottom-10-px">
             <span class="z-font-size-17 z-color-333 z-margin-bottom-3-px z-font-weight-bold">{{item.title}}</span>
           </div>
-          <img class="z-img-contain" :src="newSrc" />
+          <img class="z-img-contain" :src="newSrc || item.banner" />
           <div class="ub-flex-1 z-padding-left-10-px ub-box ub-col z-margin-top-6-px">
 <!--            <span class="z-font-size-12 z-color-888 z-margin-bottom-3-px" style="text-align: right;padding-right: 5px;">{{item.createTime}}</span>-->
 <!--            <span class="z-font-size-12 z-color-888 z-margin-bottom-3-px">{{item.content | ellipsis}}</span>-->
