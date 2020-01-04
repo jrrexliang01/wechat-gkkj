9d0818138ca6b072ed28d402bf23c79463f9f52a
zhangyongliang <jrrexliang@live.com>
2020-01-04 17:10:29 +0800

    添加病例列表

---
+++ src/pages/caseList/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container ub-box">
+    <scroll-view scroll-y style="height:calc(100vh);" scroll-top="0" v-if="caseList.length !== 0">
+      <dl class="z-width-100-percent ub-box ub-col">
+        <dd @click.stop="$openWin('/pages/patInfo/main?reportId=' + val.id)" v-for="(val, idx) in caseList" :key="idx"
+            class="order z-width-100-percent ub-box z-box-sizing-border">
+          <div class="ub-flex-1 z-padding-left-10-px ub-box ub-col">
+            <span
+              class="z-font-size-15 z-color-333 z-margin-bottom-3-px z-font-weight-bold">{{val.patient.alias}}</span>
+            <span class="z-font-size-12 z-color-888 z-margin-bottom-3-px">日期:{{val.createTime}}</span>
+          </div>
+        </dd>
+      </dl>
+      <i-tab-bar :current="current" color="#357cfb" @change="handleChange" fixed="true">
+        <i-tab-bar-item key="chat" icon="interactive" current-icon="interactive_fill" title="病例"></i-tab-bar-item>
+        <i-tab-bar-item key="homepage" icon="homepage" current-icon="homepage_fill" title="首页"></i-tab-bar-item>
+        <i-tab-bar-item key="science" icon="document" current-icon="document_fill" title="科普"></i-tab-bar-item>
+        <i-tab-bar-item key="mine" icon="mine" current-icon="mine_fill" title="我的"></i-tab-bar-item>
+      </i-tab-bar>
+    </scroll-view>
+  </div>
+</template>
+
+<script>
+  import {getCaseList} from '../../config'
+  import {switchUserTab} from '../../utils/common'
+
+  export default {
+    data () {
+      return {
+        caseList: {},
+        current: 'chat',
+        user: {}
+      }
+    },
+    async onLoad () {
+      this.user = wx.getStorageSync('userInfo')
+      const {caseList} = await getCaseList(parseInt(this.user.id))
+      this.caseList = caseList
+    },
+    async onShow () {
+      this.current = 'chat'
+    },
+    methods: {
+      handleChange (detail) {
+        switchUserTab(this.current, detail)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .container {
+    width: 100%;
+    height: 100vh;
+    background: #fff
+  }
+
+  .order {
+    border-bottom: 1px solid #f5f5f5;
+    padding: 10px 8px;
+  }
+
+  .order img {
+    width: 55px;
+    height: 55px;
+    border-radius: 3px
+  }
+</style>
