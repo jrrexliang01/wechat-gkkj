5f2353311205cfd487982b362808161938337d64
zhangyongliang <jrrexliang@gmail.com>
2020-01-13 12:12:28 +0800

    Revert "修改医生注册吗及患者注册的关联关系"
    This reverts commit fcbaa9698b40589a02d72358117b1d5758e8dc71.

--- src/pages/init/index.vue
+++ src/pages/init/index.vue
@@ -7,51 +7,71 @@
 </template>
 
 <script>
-import {enclosureFindAll, getBaseDocs} from '../../config'
-export default {
-  onLoad () {
-    wx.clearStorage()
-    wx.login({
-      success (res) {
-        if (res.code) {
-          wx.request({
-            url: 'https://gkkj.jrrexliang.com/wx/user/wx8610e4f0f0c5f3bb/login',
-            data: {
-              code: res.code
-            },
+  import {mapState} from 'vuex'
+  import {genTestUserSig} from '../../../static/utils/GenerateTestUserSig'
+  import {enclosureFindAll, getBaseDocs} from '../../config'
+
+  export default {
+    computed: {
+      ...mapState({
+        isSdkReady: state => {
+          return state.global.isSdkReady
+        }
+      })
+    },
+    onLoad () {
+      wx.clearStorage()
+      wx.login({
+        success (res) {
+          if (res.code) {
+            wx.request({
+              url: 'https://gkkj.jrrexliang.com/wx/user/wx8610e4f0f0c5f3bb/login',
+              data: {
+                code: res.code
+              },
             method: 'GET',
             success (res) {
               var sessionKey = res.data
               wx.setStorageSync('sessionKey', sessionKey)
               wx.setStorageSync('token', sessionKey.unionid)
-              wx.request({
-                url: 'https://gkkj.jrrexliang.com/api/wx/doc/userType',
-                data: {
-                  openId: sessionKey.openid
-                },
-                header: {
-                  'content-type': 'application/json', // 默认值
-                  'wxAuthorization': 'Bearer ' + wx.getStorageSync('token')
-                },
-                method: 'POST',
-                success (res) {
-                  if (res.data.data.userType === null) {
-                    wx.redirectTo({
-                      url: '/pages/home/main'
-                    })
-                  } else if (res.data.data.userType === 1) {
-                    wx.setStorageSync('userInfo', res.data.data.doc)
-                    wx.redirectTo({
-                      url: '/pages/doc/home/main'
-                    })
-                  } else if (res.data.data.userType === 2) {
-                    wx.setStorageSync('userInfo', res.data.data.patient)
-                    wx.redirectTo({
-                      url: '/pages/home/main'
-                    })
-                  }
-                }
-              })
+              let options = genTestUserSig(sessionKey.openid)
+              options.runLoopNetType = 0
+              if (options) {
+                wx.$app.login({
+                  userID: sessionKey.openid,
+                  userSig: options.userSig,
+                  hasUserInfo: true
+                }).then(() => {
+                  wx.request({
+                    url: 'https://gkkj.jrrexliang.com/api/wx/doc/userType',
+                    data: {
+                      openId: sessionKey.openid
+                    },
+                    header: {
+                      'content-type': 'application/json', // 默认值
+                      'wxAuthorization': 'Bearer ' + wx.getStorageSync('token')
+                    },
+                    method: 'POST',
+                    success (res) {
+                      if (res.data.data.userType === null) {
+                        wx.redirectTo({
+                          url: '/pages/home/main'
+                        })
+                      } else if (res.data.data.userType === 1) {
+                        wx.setStorageSync('userInfo', res.data.data.doc)
+                        wx.redirectTo({
+                          url: '/pages/doc/home/main'
+                        })
+                      } else if (res.data.data.userType === 2) {
+                        wx.setStorageSync('userInfo', res.data.data.patient)
+                        wx.redirectTo({
+                          url: '/pages/home/main'
+                        })
+                      }
+                    }
+                  })
+                })
+              }
             }
           })
         }
